931b7e6e98a9232f96f3dd51ad0bdca23af4d6ca
A-hu <louis0911317399@gmail.com>
2017-03-23 16:23:37 +0800

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,6 @@ puts "5 skills created"
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "RubyonRails",
     body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
   )
 end
 
@@ -46,3 +44,11 @@ end
 end
 
 puts "9 portfolios created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+                     name: "Technology #{technology}"
+                    )
+end
+
+puts "3 technologies created"
