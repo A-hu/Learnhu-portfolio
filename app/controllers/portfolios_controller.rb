9c4c28999b2687eca705da2a2a1f51d32f5283b6
A-hu <louis0911317399@gmail.com>
2017-05-25 18:25:50 +0800

    Added guest user for current user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -46,7 +46,7 @@ class PortfoliosController < ApplicationController
 
   def destroy
     @portfolio_item = Portfolio.find(params[:id])
-    @portfolio_item.destroy 
+    @portfolio_item.destroy
 
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'This portfolio successfully deleted' } 
