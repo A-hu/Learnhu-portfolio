407018d52e5773d2fae2ed03fbe81a8e8fce99b4
A-hu <louis0911317399@gmail.com>
2017-05-26 10:43:05 +0800

    Added nav bar, and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Learnhu Portfolio | Devcamp"
+    @seo_keywords = "Louis Tseng portfolio"
+  end
+end
--- app/controllers/concerns/set_source.rb
+++ app/controllers/concerns/set_source.rb
@@ -1,7 +1,7 @@
 module SetSource
   extend ActiveSupport::Concern
 
-  included do 
+  included do
     before_action :set_source
   end
 
