673836e4b464f31f80ad111d13bb5c95ae211a14
A-hu <louis0911317399@gmail.com>
2017-06-13 16:27:31 +0800

    Updated login navbar

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
-  def gravatar_helper user
-    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  def gravatar_helper user, width, type = ''
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: width, class: type
   end
 
   class CodeRayify < Redcarpet::Render::HTML
