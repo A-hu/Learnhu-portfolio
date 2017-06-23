e92ce15e480a57974219605610b67bb834aaf2c2
A-hu <louis0911317399@gmail.com>
2017-06-23 17:13:59 +0800

    Implemented delete to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
-  before_action :check_current_user, only: [:like]
+  before_action :check_current_user
+  before_action :find_comment, except: [:create]
+  before_action :check_author, only: [:destroy]
 
   def create
     @comment = current_user.comments.build(comment_params)
   end
 
   def like
-    @comment = Comment.find params[:id]
     like_comments = current_user.like_comments
     like_comments.include?(@comment) ? like_comments.delete(@comment) : like_comments << @comment
 
@@ -16,7 +17,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    blog = @comment.blog
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to blog, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+
+  def find_comment
+    @comment = Comment.find params[:id]
+  end
+
+  def check_author
+    if @comment.user == current_user || logged_in?(:site_admin)
+      @comment
+    else
+      redirect_to @comment.blog, notice: 'Permission denied'
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
