62147a0510e2f9da99901d928951d8053d836eeb
A-hu <louis0911317399@gmail.com>
2017-06-23 18:07:50 +0800

    Implemented topics subscription automatically

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,9 +2,10 @@ class Topic < ApplicationRecord
   validates_presence_of :title
   validates_uniqueness_of  :title
   has_many :blogs
-
   has_many :like_joins, as: :likable, dependent: :destroy
   has_many :like_users, source: :user, through: :like_joins, as: :topics, dependent: :destroy
+
+  after_create :auto_subscription
 
   def self.with_blogs
     includes(:blogs).where.not(blogs: { id: nil }).where.not(blogs: { status: 0 }).order(updated_at: :desc)
@@ -12,5 +13,11 @@ class Topic < ApplicationRecord
 
   def self.with_weekly_blogs
     includes(:blogs).where.not(blogs: { status: 0}).where(blogs: {created_at: (Time.now - 7.days)..Time.now})
+  end
+
+  def auto_subscription
+    User.all.each do |user|
+      user.like_topics << self
+    end
   end
 end
